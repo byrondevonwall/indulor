ceb49ed8c460e6f9d55391a2b41a357c8ea749cc
Byron Wall <byrondevonwall@gmail.com>
2016-03-02 17:01:43 -0500

    add pages to navigate, finish rough index.html, start playing with google maps

---
+++ assets/js/scripts.js
@@ -0,0 +1,4 @@
+$(".maps").append(
+  '<img src="https://maps.googleapis.com/maps/api/staticmap?center=Graham,NC&zoom=14&size=400x400&scale=2&maptype=terrain&key=AIzaSyClA-RGKExaGrtNirfY2T1BT5BjUfiDOCI" alt="indulor locations" width="90%"></img>'
+
+)
