a46df9875cfb2c284a1825fbfa43ba9339301de9
Byron Wall <byrondevonwall@gmail.com>
2016-03-02 19:11:32 -0500

    add google search bar, add product names

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,4 @@
 $(".maps").append(
-  '<img src="https://maps.googleapis.com/maps/api/staticmap?center=Graham,NC&zoom=14&size=400x400&scale=2&maptype=terrain&key=AIzaSyClA-RGKExaGrtNirfY2T1BT5BjUfiDOCI" alt="indulor locations" width="90%"></img>'
+  '<img class= "embed-responsive-item" src="https://maps.googleapis.com/maps/api/staticmap?center=0,0&zoom=1&size=400x400&scale=2&maptype=terrain&markers=size:tiny%7Ccolor:red%7Cankum,germany%7Cbramsche,germany%7CZug,switzerland%7Cgraham,nc%7Cnewark,nj%7Csaint+laurent,canada%7Cnaucalpan,mexico%7Cmedellin,colombia%7Csao+paulo,brazil%7Cbuenos+aires,argentina%7Calexandria,egypt%7Cbarcelona,spain%7Cshropshire,england%7Chuddersfield,england%7Cwarsaw,poland%7Cbitterfield,germany%7Cpfungstadt,germany%7Ctrezzano+sul+naviglio,italy%7Crho-mi,italy%7Ccacak,serbia%7Csharjah,UAE%7Cmumbai,india%7Cbangkok,thailand%7Cselangor,malaysia%7Chong+kong,china%7Cfoshan,china%7Cshanghai,china&key=AIzaSyClA-RGKExaGrtNirfY2T1BT5BjUfiDOCI" alt="indulor locations"></img>'
 
 )
